dcb786051eb03e59a9119c37546e43973474d5c0
tatematsu-k <ynu1264@gmail.com>
2020-12-09 20:22:37 +0900

    refix for message

--- app/models/line_event_manager/message/text.rb
+++ app/models/line_event_manager/message/text.rb
@@ -1,5 +1,22 @@
 class LineEventManager
   class Message::Text < Message
     attribute :text, String
+
+    def call
+      client.reply_message(reply_token, message_attributes)
+    end
+
+    private
+
+    def client
+      LineClientFactory.get
+    end
+
+    def message_attributes
+      {
+        type: 'text',
+        text: text,
+      }
+    end
   end
 end
--- app/models/line_events_manager.rb
+++ app/models/line_events_manager.rb
@@ -3,24 +3,7 @@ class LineEventsManager < ApplicationModel
 
   def call
     events.each do |event|
-      line_source = LineSource.factory(event["source"])
-      case event
-      when Line::Bot::Event::Follow
-        # User.create!
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
-      end
+      LineEventManager.new(event: event).call
     end
   end
 end
