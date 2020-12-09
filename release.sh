1a9cfc199903051067d531e64ad3617dabb91cf4
tatematsu-k <ynu1264@gmail.com>
2020-12-10 08:33:06 +0900

    refix release script

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 mkdir deploy
 cd deploy
 git clone https://github.com/tatematsu-k/sakurada_christmas_2020.git
+cp .env.development.remote sakurada_christmas_2020/.env.development.remote
 cd sakurada_christmas_2020
 AWS_PROFILE=sandbox jets deploy
 cd ../..
