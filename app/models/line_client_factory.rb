c68f887b2de4551fd685a7c51c6341c858e42fa4
tatematsu-k <ynu1264@gmail.com>
2020-12-10 09:05:08 +0900

    refix line config

--- app/models/line_client_factory.rb
+++ app/models/line_client_factory.rb
@@ -1,6 +1,7 @@
 class LineClientFactory
   def self.get
     @@line_client ||= Line::Bot::Client.new do |config|
+      config.channel_id = ENV["LINE_CHANNEL_ID"]
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     end
