4492a3b43189e32b58758acd0aa8175b6f2abad7
tatematsu-k <ynu1264@gmail.com>
2020-12-11 10:52:11 +0900

    add migration routing

---
+++ app/controllers/migrations_controller.rb
@@ -0,0 +1,36 @@
+require 'open3'
+class MigrationsController < ApplicationController
+  InvalidTokenError = Class.new(StandardError)
+  before_action :authenticate
+
+  def create
+    run_cmd!("jets db:create db:migrate")
+
+    render json: { status: "success" }
+  end
+
+  private
+
+  def authenticate
+    result ||=
+      authenticate_with_http_basic do |token, _|
+        token == ENV["MIGRATION_TOKEN"]
+      end
+    raise InvalidTokenError unless result
+  end
+
+  def run_cmd!(cmd)
+    Open3.capture3(cmd).tap do |stdout,stderr,status|
+      puts stdout
+      raise CommandError, "#{cmd} is failed." unless status.success?
+    end
+  end
+
+  def authenticate_with_http_basic
+    authorization = (headers["authorization"] || headers["Authorization"])&.gsub(/^Basic /, "")
+    return nil unless authorization
+
+    user, pass = Base64.decode64(authorization).split(":")
+    yield(user, pass)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Jets.application.routes.draw do
   post 'webhooks/create'
+  post 'migrations/create'
 end
