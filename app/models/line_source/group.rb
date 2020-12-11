ff3808441e8a6e4a5cf1150781df0f6db8eac910
tatematsu-k <ynu1264@gmail.com>
2020-12-11 17:27:35 +0900

    add Group save

--- app/models/line_source/group.rb
+++ app/models/line_source/group.rb
@@ -3,5 +3,12 @@ class LineSource
     attribute :groupId
 
     alias group_id groupId
+
+    def save!
+      ActiveRecord::Base.transaction do
+        ::User.find_or_create_by!(line_uid: user_id)
+        ::Group.find_or_create_by!(group_uid: group_id)
+      end
+    end
   end
 end
