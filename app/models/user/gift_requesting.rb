bbb3ee10dfd029f9da75eb7ed2bb4bf7af9d23cb
tatematsu-k <ynu1264@gmail.com>
2020-12-12 16:12:26 +0900

    add dependent destroy

--- app/models/user/gift_requesting.rb
+++ app/models/user/gift_requesting.rb
@@ -3,11 +3,13 @@ class User::GiftRequesting < ApplicationRecord
   has_one :group_message_using,
           class_name: "User::GiftRequesting::GroupMessageUsing",
           inverse_of: :user_gift_requesting,
-          foreign_key: :user_gift_requesting_id
+          foreign_key: :user_gift_requesting_id,
+          dependent: :destroy
   has_one :private_message_using,
           class_name: "User::GiftRequesting::PrivateMessageUsing",
           inverse_of: :user_gift_requesting,
-          foreign_key: :user_gift_requesting_id
+          foreign_key: :user_gift_requesting_id,
+          dependent: :destroy
 
   def group_messaged?
     group_message_using.present?
