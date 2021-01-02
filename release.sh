8a9ea6387bb4b8bbc96c70b5cf0949b6d20d5ac5
tatematsu-k <ynu1264@gmail.com>
2021-01-02 19:04:44 +0900

    Revert "refix to public network"
    This reverts commit 71ff9766c49fe83c291eba242fd8c4cddd1616ab.

--- release.sh
+++ release.sh
@@ -4,6 +4,6 @@ cd deploy
 git clone https://github.com/tatematsu-k/sakurada_christmas_2020.git
 cp ../.env.production.remote sakurada_christmas_2020/.env.production.remote
 cd sakurada_christmas_2020
-AWS_PROFILE=sandbox JETS_ENV=production jets deploy
+AWS_PROFILE=sandbox JETS_ENV=production SECURITY_GROUP_IDS=sg-07544e143de07134f SUBNET_IDS=subnet-0f2b297ed57931135,subnet-0a7eb6c8633fd139d jets deploy
 cd ../..
 rm -rf deploy
